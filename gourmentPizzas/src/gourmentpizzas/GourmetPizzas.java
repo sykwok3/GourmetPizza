895d6fa4a1c61c0f526c72dcee85a65ccce90e25
sykwok3 <43140960+sykwok3@users.noreply.github.com>
2022-11-10 18:30:23 +0000

    Add files via upload

---
+++ gourmentPizzas/src/gourmentpizzas/GourmetPizzas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gourmentpizzas;
+
+/**
+ *
+ * @author user
+ */
+public class GourmetPizzas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewOrNotForm form1 = new NewOrNotForm();
+        form1.setVisible(true);
+    }
+
+}
